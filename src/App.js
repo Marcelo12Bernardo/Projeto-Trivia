0d4903b4aca308654e36ccd1b43f450b9e152f02
Thiago Xavier <thiagox58@gmail.com>
2023-02-09 17:36:45 -0300

    requisito-03

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Game from './pages/Game';
 import Login from './pages/Login';
+import Settings from './pages/Settings';
 
 class App extends Component {
   render() {
     return (
       <Switch>
+        <Route path="/settings" component={ Settings } />
         <Route path="/game" component={ Game } />
         <Route exact path="/" component={ Login } />
       </Switch>
---
+++ src/pages/Settings.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Settings extends Component {
+  render() {
+    return (
+      <main>
+        <h1 data-testid="settings-title">
+          Setings Page
+        </h1>
+
+      </main>
+    );
+  }
+}
+
+export default Settings;
