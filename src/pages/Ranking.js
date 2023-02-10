b77a275b8c3e3838ce232d0ac38cf8bb6af1177b
Thiago Xavier <thiagox58@gmail.com>
2023-02-10 17:01:43 -0300

    requisito-18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,15 +1,34 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 class Ranking extends Component {
+  returnLogin = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <main>
         <h1 data-testid="ranking-title">
           Ranking Page
         </h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.returnLogin }
+        >
+          Voltar ao Inicio
+        </button>
       </main>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
 
 export default Ranking;
