de83ba1cbd6a5d574afb5299484b76c036adb012
Thiago Xavier <thiagox58@gmail.com>
2023-02-10 16:35:52 -0300

    requisito-12

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 
-class Game extends Component {
+class Feedback extends Component {
   render() {
     return (
       <div>
         <Header />
-        <h1>
-          Game Page
-        </h1>
       </div>
+
     );
   }
 }
 
-export default Game;
+export default Feedback;
