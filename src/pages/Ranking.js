eb31bb0103b910308284fa5af2b97aa14ddc051c
Thiago Xavier <thiagox58@gmail.com>
2023-02-10 16:52:46 -0300

    requisito-16

---
+++ src/pages/Ranking.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Ranking extends Component {
+  render() {
+    return (
+      <main>
+        <h1 data-testid="ranking-title">
+          Ranking Page
+        </h1>
+      </main>
+    );
+  }
+}
+
+export default Ranking;
